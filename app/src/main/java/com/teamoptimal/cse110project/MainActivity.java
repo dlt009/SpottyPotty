58c12840bf8c90470d9136893c3932312fd0077c
k5Lchu <k3chu@ucsd.edu>
2016-02-20 13:23:47 -0800

    Shared Preferences added

--- app/src/main/java/com/teamoptimal/cse110project/MainActivity.java
+++ app/src/main/java/com/teamoptimal/cse110project/MainActivity.java
@@ -2,6 +2,7 @@ package com.teamoptimal.cse110project;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
