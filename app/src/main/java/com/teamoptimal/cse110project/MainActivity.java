b6964ba10d098b1896a19d72c65caac6855ad588
k5Lchu <k3chu@ucsd.edu>
2016-02-21 23:25:55 -0800

    Created ListView in Nav Menu

--- app/src/main/java/com/teamoptimal/cse110project/MainActivity.java
+++ app/src/main/java/com/teamoptimal/cse110project/MainActivity.java
@@ -205,13 +205,6 @@ public class MainActivity extends AppCompatActivity
 
     private void goToCreateRestroom() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
 
